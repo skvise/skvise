f5594de06bf39714b5a6f9561073e5c34c421fe2
Skvise <skvise@i.ua>
2021-02-27 21:42:48 +0200

    test

---
+++ day2/Untitled-2.js
@@ -0,0 +1,25 @@
+//             0       1       2
+// let arr = [123,'asfdadad',456,[1,2,3]]
+
+// let user = {
+//     name: 'kokos',
+//     age: 123
+// }
+// console.log();
+
+// let users = [user1,user2,user3,user4];
+
+// console.log(users);
+
+let prog = {
+    name: 'alex',
+    age: 65,
+    skills: ['js','java','php']
+    wife:{
+        name: 'anita';
+        age: 18;
+        skills:['c++','mysql']
+    }
+};
+
+console.log(prog.skills[2]);
